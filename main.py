3a3721e05574c0dd0f179eaff267053aff077a39
Lachin Ashirova <60476768+Lashirova@users.noreply.github.com>
2021-11-05 04:07:37 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 
-def Subsets(arr, n, v, sum) :
+def Subsets(arr, n, val, sum) :
 	if (sum == 0) :
-		for value in v :
+		for value in val :
 			print(value, end=" ")
 		print()
 		return
 	if (n == 0):
 		return
-	Subsets(arr, n - 1, v, sum)
-	v1 = [] + v
+	Subsets(arr, n - 1, val, sum)
+	v1 = [] + val
 	v1.append(arr[n - 1])
 	Subsets(arr, n - 1, v1, sum - arr[n - 1])
 
 def AllSubsets(arr, n, sum):
-	v = []
-	Subsets(arr, n, v, sum)
+	val = []
+	Subsets(arr, n, val, sum)
 
 arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
 sum = 23
